af810b5d1b5279fac2deb851206f0e1ce1ed04fb
Derick Ostrenko <dostrenko@lsu.edu>
2024-09-16 14:48:37 -0500

    connectsion

--- sketch.js
+++ sketch.js
@@ -1,6 +1,54 @@
 function setup() {
-  createCanvas(800, 600);
+
+  createCanvas(400, 400);
   background(100);
+
+  //////////
+  // GRID //
+  //////////
+
+  // var circleSize = 3;
+
+  // for(var i = circleSize/2; i < width; i = i + circleSize*1.25){
+  //   for(var n = circleSize/2; n < height; n = n + circleSize*1.25) {
+  //     ellipse(i, n, circleSize);
+  //   }
+  // }
+
+  /////////////////
+  // CONNECTIONS //
+  /////////////////
+
+  scale(0.1);
+  translate(width*4, height*3.5);
+  
+  for(var i = 0; i < 100; i++) {
+    translate(width/2, i*4);
+    // scale(1+i/1000);
+    rotate(PI/8);
+    beginShape();
+    vertex(width/2, height/2);
+    vertex(width, height);
+    vertex(0, height);
+    endShape();
+
+    beginShape();
+    vertex(0, 0);
+    vertex(width, height);
+    endShape();
+
+    strokeWeight(i);
+    // strokeWeight(i*5);
+
+
+    fill(i)
+
+  }
+
+  //////////////
+  // GRADIENT //
+  //////////////
+
 }
 
 function draw() {
