5b3203cccbe28600f65672f312033bcc760cd637
Derick Ostrenko <dostrenko@lsu.edu>
2024-09-16 14:57:20 -0500

    gradient

--- sketch.js
+++ sketch.js
@@ -19,35 +19,42 @@ function setup() {
   // CONNECTIONS //
   /////////////////
 
-  scale(0.1);
-  translate(width*4, height*3.5);
+  // scale(0.1);
+  // translate(width*4, height*3.5);
   
-  for(var i = 0; i < 100; i++) {
-    translate(width/2, i*4);
-    // scale(1+i/1000);
-    rotate(PI/8);
-    beginShape();
-    vertex(width/2, height/2);
-    vertex(width, height);
-    vertex(0, height);
-    endShape();
+  // for(var i = 0; i < 100; i++) {
+  //   translate(width/2, i*4);
+  //   // scale(1+i/1000);
+  //   rotate(PI/8);
+  //   beginShape();
+  //   vertex(width/2, height/2);
+  //   vertex(width, height);
+  //   vertex(0, height);
+  //   endShape();
 
-    beginShape();
-    vertex(0, 0);
-    vertex(width, height);
-    endShape();
+  //   beginShape();
+  //   vertex(0, 0);
+  //   vertex(width, height);
+  //   endShape();
 
-    strokeWeight(i);
-    // strokeWeight(i*5);
+  //   strokeWeight(i);
+  //   // strokeWeight(i*5);
 
 
-    fill(i)
+  //   fill(i)
 
-  }
+  // }
 
   //////////////
   // GRADIENT //
   //////////////
+
+  // strokeWeight(10);
+
+  for(var x = 0; x < width+1; x++) {
+    line(x, 0, x, height);
+    stroke(x);
+  }
 
 }
 
